84b58bbf31282e27c1eda3f68d9a4911754bc7ef
Jack Shi <jack@ofload.com>
2024-05-05 00:52:02 +1000

    adding chatbot to nextjs and have it call localhost:5000

--- packages/lamaindex/app.py
+++ packages/lamaindex/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import string
 from llama_index.core import StorageContext, load_index_from_storage
 
 # My OpenAI Key
 import os
-os.environ['OPENAI_API_KEY'] = "ADD YOUR CHAT GTP KEY HERE"
+os.environ['OPENAI_API_KEY'] = "GTP API KEY"
 
 # rebuild storage context
 storage_context = StorageContext.from_defaults(persist_dir="./index/ai_index")
@@ -14,9 +15,10 @@ index = load_index_from_storage(storage_context)
 query_engine = index.as_query_engine()
 
 app = Flask("EthID ChatBot")
-
+CORS(app) # This will enable CORS for all routes
+#CORS(app, resources={r"/ask": {"origins": "http://localhost:3000"}})
 nounce_vibe = "Nouns DAO has a vibe that's both innovative and community-oriented. It's like a bustling marketplace of creativity and collaboration, where people come together to tokenize and trade nouns. There's a sense of excitement and experimentation, with members actively participating in shaping the direction of the DAO and exploring the possibilities of decentralized ownership and governance. It's a space where imagination meets technology, fostering a culture of exploration and expression."
-nounce_vibe_prompt = nounce_vibe + " In this vibe, answer the following question: \n\n"
+nounce_vibe_prompt = nounce_vibe + " In this vibe, answer the following question in a short chat bot response: \n\n"
 
 
 @app.route("/")
--- packages/nextjs/components/chat/chat.jsx
+++ packages/nextjs/components/chat/chat.jsx
@@ -1,47 +1,151 @@
-"use client";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ChatBot, { Loading } from 'react-simple-chatbot';
 
-import React from "react";
-import ChatBot from "react-simple-chatbot";
-import { ThemeProvider } from "styled-components";
 
-// all available props
-const theme = {
-  background: "#f5f8fb",
-  fontFamily: "Helvetica Neue",
-  headerBgColor: "#EF6C00",
-  headerFontColor: "#fff",
-  headerFontSize: "15px",
-  botBubbleColor: "#EF6C00",
-  botFontColor: "#fff",
-  userBubbleColor: "#fff",
-  userFontColor: "#4a4a4a",
-};
+function AIClient() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-const Chat = () => {
-  const steps = [
-    {
-      id: "0",
-      message: "Welcome to react chatbot!",
-      trigger: "1",
-    },
-    {
-      id: "1",
-      message: "Bye!",
-      end: true,
-    },
-  ];
+  useEffect(() => {
+    const self = this;
+    const { steps } = this.props;
+    const search = steps.search.value;
 
-  // const [conversationList, setConversationList] = useState([{ name: "bot", message: "how can I help you?" }]);
-
-  // const [message, setMessage] = useState("");
+    fetch('https://httpbin.org/get')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  }, []);
 
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <ChatBot steps={steps} />
-      </ThemeProvider>
-    </>
+    <div>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
+    </div>
   );
+}
+
+
+
+class DBPedia extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      result: '',
+      trigger: false,
+    };
+
+    this.triggetNext = this.triggetNext.bind(this);
+  }
+
+  componentWillMount() {
+    const self = this;
+    const { steps } = this.props;
+    const search = steps.search.value;
+    //const endpoint = encodeURI('http://localhost:5000');
+    const query = encodeURI(`${search}`);
+
+    const queryUrl = `http://localhost:5000/ask?q=${query}`;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('readystatechange', readyStateChange);
+
+    function readyStateChange() {
+      if (this.readyState === 4) {
+        const data = this.responseText;
+        console.log("results: "+ data);
+        if(data){
+          self.setState({ loading: false, result: data });
+        } else {
+          self.setState({ loading: false, result: 'Not found.' });
+        }
+        /*
+        const data = JSON.parse(this.responseText);
+        console.log("results: "+ data)
+        const bindings = data.results.bindings;
+        if (bindings && bindings.length > 0) {
+          self.setState({ loading: false, result: bindings[0].comment.value });
+        } else {
+          self.setState({ loading: false, result: 'Not found.' });
+        }
+        */
+      }
+    }
+
+    xhr.open('GET', queryUrl);
+    console.log("query URL " + queryUrl);
+    xhr.send();
+  }
+
+  triggetNext() {
+    this.setState({ trigger: true }, () => {
+      this.props.triggerNextStep();
+    });
+  }
+
+  render() {
+    const { trigger, loading, result } = this.state;
+
+    return (
+      <div className="dbpedia">
+        { loading ? <Loading /> : result }
+        {
+          !loading &&
+          <div
+            style={{
+              textAlign: 'center',
+              marginTop: 20,
+            }}
+          >
+            {
+              !trigger &&
+              <button
+                onClick={() => this.triggetNext()}
+              >
+                Search Again
+              </button>
+            }
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+DBPedia.propTypes = {
+  steps: PropTypes.object,
+  triggerNextStep: PropTypes.func,
 };
+
+DBPedia.defaultProps = {
+  steps: undefined,
+  triggerNextStep: undefined,
+};
+
+const Chat = () => (
+  <ChatBot
+    steps={[
+      {
+        id: '1',
+        message: 'Type something to search on Wikipédia. (Ex.: Brazil)',
+        trigger: 'search',
+      },
+      {
+        id: 'search',
+        user: true,
+        trigger: '3',
+      },
+      {
+        id: '3',
+        component: <DBPedia />,
+        waitAction: true,
+        trigger: '1',
+      },
+    ]}
+  />
+);
 
 export default Chat;
