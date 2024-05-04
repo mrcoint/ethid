33194457b42484d2746192a73ca15b80b9b78b91
Jack Shi <jack@ofload.com>
2024-05-04 22:40:40 +1000

    Added chat bot

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,34 +2,41 @@
 
 import Link from "next/link";
 import type { NextPage } from "next";
+import Chat from "~~/components/chat/chat";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="flex items-left flex-col flex-grow pt-10 sm:px-5 md:px-10 lg:px-20 bg-[url('/background-kids.jpeg')] bg-cover">
-        <div className="px-5">
-          <h1 className="text-left my-5">
-            <span className="block text-4xl font-bold">Every $1 Counts</span>
-          </h1>
-          <p className="text-left text-lg my-0">We&apos;re changing the lives of people in need,</p>
-          <p className="text-left text-lg my-0">1 coffee at a time.</p>
+      <div className="flex flex-row bg-[url('/background-kids.jpeg')] bg-cover">
+        <div className="flex items-left flex-col flex-grow pt-10 sm:px-5 md:px-10 lg:px-20 ">
+          <div className="px-5">
+            <h1 className="text-left my-5">
+              <span className="block text-4xl font-bold">Every $1 Counts</span>
+            </h1>
+            <p className="text-left text-lg my-0">We&apos;re changing the lives of people in need,</p>
+            <p className="text-left text-lg my-0">1 coffee at a time.</p>
+          </div>
+
+          <div className="flex-grow w-full mt-16 px-8">
+            <div className="flex justify-center items-left gap-12 flex-col sm:flex-column">
+              <Link href="/claim" passHref className="link">
+                <div className="flex flex-col bg-base-100 px-5 py-2 text-center items-center max-w-48 rounded-3xl">
+                  {/* <GiftIcon className="h-8 w-8 fill-secondary" /> */}
+                  <p>Make a claim</p>
+                </div>
+              </Link>
+              <Link href="/offramp" passHref className="link">
+                <div className="flex flex-col bg-base-100 px-5 py-2 text-center items-center max-w-48 rounded-3xl">
+                  {/* <CurrencyDollarIcon className="h-8 w-8 fill-secondary" /> */}
+                  <p>Get paid</p>
+                </div>
+              </Link>
+            </div>
+          </div>
         </div>
 
-        <div className="flex-grow w-full mt-16 px-8">
-          <div className="flex justify-center items-left gap-12 flex-col sm:flex-column">
-            <Link href="/claim" passHref className="link">
-              <div className="flex flex-col bg-base-100 px-5 py-2 text-center items-center max-w-48 rounded-3xl">
-                {/* <GiftIcon className="h-8 w-8 fill-secondary" /> */}
-                <p>Make a claim</p>
-              </div>
-            </Link>
-            <Link href="/offramp" passHref className="link">
-              <div className="flex flex-col bg-base-100 px-5 py-2 text-center items-center max-w-48 rounded-3xl">
-                {/* <CurrencyDollarIcon className="h-8 w-8 fill-secondary" /> */}
-                <p>Get paid</p>
-              </div>
-            </Link>
-          </div>
+        <div className="p-10">
+          <Chat />
         </div>
       </div>
     </>
---
+++ packages/nextjs/components/chat/chat.jsx
@@ -0,0 +1,48 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+
+// all available props
+const theme = {
+  background: '#f5f8fb',
+  fontFamily: 'Helvetica Neue',
+  headerBgColor: '#EF6C00',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#EF6C00',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+};
+
+const Chat = () => {
+
+    const steps = [
+        {
+          id: '0',
+          message: 'Welcome to react chatbot!',
+          trigger: '1',
+        },
+        {
+          id: '1',
+          message: 'Bye!',
+          end: true,
+        },
+      ];
+
+    const [conversationList, setConversationList] = useState([{name: 'bot', message: 'how can I help you?'}]);
+
+    const [message, setMessage] = useState("");
+
+    return (
+        <>
+          <ThemeProvider theme={theme}>
+            <ChatBot steps={steps} />
+          </ThemeProvider>
+        </>
+    );  
+};
+
+
+export default Chat;
